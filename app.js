9e68ee3731c10ff1a3f848e4f8cb781001bb18ea
Dan <the.code.writer@gmail.com>
2014-02-03 19:52:02 -0500

    run on port 80 for now

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
-app.listen(8080);
-console.log('Listening on port 8080');
+app.listen(80);
+console.log('Listening on port 80');
 
