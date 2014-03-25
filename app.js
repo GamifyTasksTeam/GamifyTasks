7981adf877ccbaf1b7260d594f7277e4604dac1f
Dan M <the.code.writer@gmail.com>
2014-03-24 22:56:14 -0600

    Separated sections of views into different files.  Moved js into dir 'public' (all publicly accessible files should be in that directory.)

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use("/js", express.static(__dirname + '/js'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
