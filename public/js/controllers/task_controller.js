76314b6cd487bc4655ce782a8f1cd4158560702f
bravefoot <b.v.campbell3@gmail.com>
2014-03-26 15:36:14 -0600

    Tasks basics done
    The controller now allows for all of the basic things to do with tasks.

--- public/js/controllers/task_controller.js
+++ public/js/controllers/task_controller.js
@@ -1,11 +1,28 @@
 Gamify.GamifyController = Ember.ObjectController.extend({
 	actions: {
 		finishTask: function (task, wallet) {
-			console.log(wallet.get('red'));
 			wallet.set('red', wallet.get('red')+task.get('red'));
 			wallet.set('green', wallet.get('green')+task.get('green'));
 			wallet.set('blue', wallet.get('blue')+task.get('blue'));
 			task.deleteRecord();
+		},
+		
+		addTask: function(){
+			 var task = this.store.createRecord('task', {
+			task: this.get('newTitle'),
+			red: parseInt(this.get('newRed')),
+			green: parseInt(this.get('newGreen')),
+			blue: parseInt(this.get('newBlue'))
+			});
+
+		// Clear the "New Todo" text field
+		this.set('newTitle', '');
+		this.set('newRed', '');
+		this.set('newGreen', '');
+		this.set('newBlue', '');
+
+		// Save the new model
+		todo.save();
 		}
 	}
 });
