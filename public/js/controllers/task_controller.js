306fbc4a86462dadea9ca9f02e80f963127015ad
bravefoot <b.v.campbell3@gmail.com>
2014-03-30 11:42:40 -0600

    Added some mockup, will be deleted

--- public/js/controllers/task_controller.js
+++ public/js/controllers/task_controller.js
@@ -1,11 +1,25 @@
 Gamify.GamifyController = Ember.ObjectController.extend({
 	actions: {
 		finishTask: function (task, wallet) {
-			console.log(wallet.get('red'));
 			wallet.set('red', wallet.get('red')+task.get('red'));
 			wallet.set('green', wallet.get('green')+task.get('green'));
 			wallet.set('blue', wallet.get('blue')+task.get('blue'));
 			task.deleteRecord();
+		},
+		
+		addTask: function(){
+			 var task = this.store.createRecord('task', {
+			task: this.get('newTitle'),
+			red: parseInt(this.get('newRed')),
+			green: parseInt(this.get('newGreen')),
+			blue: parseInt(this.get('newBlue'))
+			});
+
+		this.set('newTitle', '');
+		this.set('newRed', '');
+		this.set('newGreen', '');
+		this.set('newBlue', '');
+		todo.save();
 		}
 	}
 });
