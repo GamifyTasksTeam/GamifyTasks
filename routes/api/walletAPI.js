902b8cb3804f0dc0d29461618cccc9669af14162
Dan M <the.code.writer@gmail.com>
2014-04-10 01:05:42 -0600

    removed unneeded return statements

--- routes/api/walletAPI.js
+++ routes/api/walletAPI.js
@@ -1,36 +1,40 @@
 var mongoose = require('mongoose');
 var Wallet = require('../../models/wallet.js').Wallet;
-exports.getWalletByID = function(req,res){
-	return Wallet.findById(req.params.id, function(err,wallet){
-		if(!err){
-			return res.send(wallet);
-		}else{
-			return console.log(err);
+exports.getWalletByID = function(req, res) {
+	return Wallet.findById(req.params.id, function(err, wallet) {
+		if (!err) {
+			res.send(wallet);
+		}
+		else {
+			console.log(err);
 		}
 	});
 }
 
-exports.getWalletByUser = function(req,res) {
-	return Wallet.findOne({'userID':req.session.userId},function(err,wallet){
-		if(!err){
-			return res.send(wallet);
-		} else {
-			return console.log(err);
-		}
-	});
-}
-
-exports.updateWalletByUser = function(req,res){	
-	return Wallet.findOne({'userID':req.session.userId},function(err,wallet){
-		wallet.green = req.body.green;
-		wallet.purple = req.body.purple;
-		wallet.red = req.body.red;
-		wallet.blue = req.body.blue;
-		return wallet.save(function(err){
-			if(err){
-				console.lof(err);
+exports.getWalletByUser = function(req, res) {
+	Wallet.findOne({ 'userID' : req.session.userId},
+		function(err, wallet) {
+			if (!err) {
+				res.send(wallet);
 			}
-			return res.send(wallet);
+			else {
+				console.log(err);
+			}
 		});
-	});
+}
+
+exports.updateWalletByUser = function(req, res) {
+	Wallet.findOne({ 'userID' : req.session.userId },
+		function(err, wallet) {
+			wallet.green = req.body.green;
+			wallet.purple = req.body.purple;
+			wallet.red = req.body.red;
+			wallet.blue = req.body.blue;
+			wallet.save(function(err) {
+				if (err) {
+					console.log(err);
+				}
+				res.send(wallet);
+			});
+		});
 }
