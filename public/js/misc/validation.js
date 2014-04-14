cd747c89b9886fac328b7d039168b8af5248ce03
Dan M <the.code.writer@gmail.com>
2014-04-13 22:22:37 -0600

    improved validation messages

--- public/js/misc/validation.js
+++ public/js/misc/validation.js
@@ -1,6 +1,9 @@
 window.ClientValidation = function() {
 	$("#newTaskForm").validate({
 		rules: {
+			newTaskName: {
+				required: true
+			},
 			newTaskRed: {
 				required: true
 			},
@@ -15,12 +18,31 @@ window.ClientValidation = function() {
 			}
 		},
 		messages: {
-			
-		}
+			newTaskName: {
+				required: "Please enter the task name"
+			},
+			newTaskRed: {
+				required: "Please enter the red points"
+			},
+			newTaskGreen: {
+				required: "Please enter the green points"
+			},
+			newTaskBlue: {
+				required: "Please enter the blue points"
+			},
+			newTaskPurple: {
+				required: "Please enter the purple points"
+			}
+		},
+		errorLabelContainer: "#newTaskFormError",
+		wrapper: "li"
 	});
 	
 	$("#newRewardForm").validate({
 		rules: {
+			newRewardName: {
+				required: true
+			},
 			newRewardRed: {
 				required: true
 			},
@@ -35,7 +57,23 @@ window.ClientValidation = function() {
 			}
 		},
 		messages: {
-			
-		}
+			newRewardName: {
+				required: "Please enter the reward name"
+			},
+			newRewardRed: {
+				required: "Please enter the red points"
+			},
+			newRewardGreen: {
+				required: "Please enter the green points"
+			},
+			newRewardBlue: {
+				required: "Please enter the blue points"
+			},
+			newRewardPurple: {
+				required: "Please enter the purple points"
+			}
+		},
+		errorLabelContainer: "#newRewardFormError",
+		wrapper: "li"
 	});
 }
