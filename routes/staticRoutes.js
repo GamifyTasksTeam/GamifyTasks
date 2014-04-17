4156948df7a496ac56a8ceaf62dc7c8282424f6f
Dan M <the.code.writer@gmail.com>
2014-04-16 23:49:08 -0600

    Added some customization to the nav bar.  Plenty more needed

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -1,19 +1,77 @@
-exports.app = function(req, res){
-    res.render('app/app', { title: 'Gamify Tasks' });
+var mongoose = require('mongoose');
+var User = require('../models/user.js').User;
+
+exports.app = function(req, res) {
+
+	User.findById(req.session.userId).exec().then(function(user) {
+		res.render('app/app', {
+			title : 'Tasks - Gamify Tasks',
+			user : user
+		});
+	}, function(err) {
+		res.send(500, {
+			error : "An error occured "
+		});
+	});
+
 };
 
-exports.index = function(req, res){
-    res.render('home', { title: 'Home - Gamify Tasks' });
+exports.index = function(req, res) {
+
+	User.findById(req.session.userId).exec().then(function(user) {
+		res.render('home', {
+			title : 'Home - Gamify Tasks',
+			user : user
+		});
+	}, function(err) {
+		res.send(500, {
+			error : "An error occured "
+		});
+	});
+
 };
 
-exports.about = function(req, res){
-    res.render('about', { title: 'About - Gamify Tasks' });
+exports.about = function(req, res) {
+
+	User.findById(req.session.userId).exec().then(function(user) {
+		res.render('about', {
+			title : 'About - Gamify Tasks',
+			user : user
+		});
+	}, function(err) {
+		res.send(500, {
+			error : "An error occured "
+		});
+	});
+
 };
 
-exports.contact = function(req, res){
-    res.render('contact', { title: 'Contact - Gamify Tasks' });
+exports.contact = function(req, res) {
+
+	User.findById(req.session.userId).exec().then(function(user) {
+		res.render('contact', {
+			title : 'Contact - Gamify Tasks',
+			user : user
+		});
+	}, function(err) {
+		res.send(500, {
+			error : "An error occured "
+		});
+	});
+
 };
 
-exports.login = function(req, res){
-    res.render('login', { title: 'Login - Gamify Tasks' });
+exports.login = function(req, res) {
+
+	User.findById(req.session.userId).exec().then(function(user) {
+		res.render('login', {
+			title : 'Login - Gamify Tasks',
+			user : user
+		});
+	}, function(err) {
+		res.send(500, {
+			error : "An error occured "
+		});
+	});
+
 };
